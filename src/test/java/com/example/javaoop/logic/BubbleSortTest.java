f134001fd8598c1e6f11764b9eb7ee40f07721e6
hoon <hoon0661@gmail.com>
2022-06-21 21:35:23 -0400

    Implement bubble sort
    bubble sort implementation & test

---
+++ src/test/java/com/example/javaoop/logic/BubbleSortTest.java
@@ -0,0 +1,26 @@
+package com.example.javaoop.logic;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleSortTest {
+
+    @DisplayName("Bubble Sort")
+    @Test
+    void given_Input_WhenExecuting_ThenReturnOutput() {
+        //Given
+        BubbleSort<Integer> bubbleSort = new BubbleSort<>();
+
+
+        //When
+        List<Integer> actual = bubbleSort.sort(List.of(3, 2, 4, 5, 1));
+
+
+        //Then
+        assertEquals(List.of(1, 2, 3, 4, 5), actual);
+    }
+}
