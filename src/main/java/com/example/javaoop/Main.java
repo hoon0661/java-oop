8cb97686bc9302d6965984873f38dbb928daa2af
hoon <hoon0661@gmail.com>
2022-06-21 22:25:53 -0400

    Update #3 - Main
    converted class to interface
    #3 finished

--- src/main/java/com/example/javaoop/Main.java
+++ src/main/java/com/example/javaoop/Main.java
@@ -1,12 +1,14 @@
 package com.example.javaoop;
 
 import com.example.javaoop.logic.BubbleSort;
+import com.example.javaoop.logic.JavaSort;
+import com.example.javaoop.logic.Sort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new JavaSort<>();
 
         System.out.println("[result] " + sort.sort(Arrays.asList(args)));
     }
